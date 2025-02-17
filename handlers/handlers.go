0d871e61a9fe255ccb338e4ab3dd4a62607aaeca
Harsh <dangiharsh@gmail.com>
2025-02-17 20:25:24 +0000

    added APIs

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,9 +1,53 @@
 package handlers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func AttachHandlers(app *fiber.App) {
-	app.Get("/:id", func(c *fiber.Ctx) error {
+	app.Get("/:key", func(c *fiber.Ctx) error {
 		return c.SendString("Hello World!")
+	})
+
+	app.Put("/", func(c *fiber.Ctx) error {
+		var putdata map[string]string
+		err := c.BodyParser(&putdata)
+		if err != nil {
+			return c.Status(400).JSON(&fiber.Map{
+				"success": true,
+				"message": "Ivalid value",
+			})
+		}
+		for k, v := range putdata {
+			fmt.Printf("%T %v %T %v\n", k, k, v, v)
+		}
+		return c.Status(200).JSON(&fiber.Map{
+			"success": true,
+			"message": "",
+		})
+	})
+
+	app.Post("/", func(c *fiber.Ctx) error {
+		var putdata map[string]string
+		err := c.BodyParser(&putdata)
+		if err != nil {
+			return c.Status(400).JSON(&fiber.Map{
+				"success": true,
+				"message": "Ivalid value",
+			})
+		}
+		for k, v := range putdata {
+			fmt.Printf("%T %v %T %v\n", k, k, v, v)
+		}
+		return c.Status(200).JSON(&fiber.Map{
+			"success": true,
+			"message": "",
+		})
+	})
+
+	app.Get("/:key", func(c *fiber.Ctx) error {
+		return c.SendString("Delete functionality pending!")
 	})
 }
