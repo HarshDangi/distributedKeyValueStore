2f50a19caf4b64a49aebaf3408b2f0f72e57e277
Harsh <dangiharsh@gmail.com>
2025-02-18 21:04:04 +0000

    fix(traverse): fixed traverseClockwise to loop around

--- consistentHashing/traverse.go
+++ consistentHashing/traverse.go
@@ -6,6 +6,10 @@ import (
 )
 
 func traverseClockwise(key [16]byte) [16]byte {
+	if bytes.Compare(ring[len(ring)-1][:], key[:]) == 1 { // if key is bigger than the largest then obviously first server would be its target i.e. looping around
+		return ring[0]
+	}
+
 	idx := sort.Search(len(ring), func(idx int) bool {
 		comp := bytes.Compare(ring[idx][:], key[:])
 		return comp == -1 || comp == 0
