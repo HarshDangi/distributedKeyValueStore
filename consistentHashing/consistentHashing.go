8b0313fd4f7c123b02a955f73713f087fac1802e
Harsh <dangiharsh@gmail.com>
2025-02-19 20:33:14 +0000

    fix(ConsistentHashing): moved from redis to json files to support hex values + minor code cleanup

--- consistentHashing/consistentHashing.go
+++ consistentHashing/consistentHashing.go
@@ -1,48 +1,85 @@
 package consistentHashing
 
 import (
+	"bytes"
 	"context"
 	"crypto/md5"
 	"encoding/hex"
-
-	"github.com/harshdangi/distributedKeyValueStore/redisClient"
-	"github.com/redis/go-redis/v9"
+	"encoding/json"
+	"log"
+	"os"
+	"slices"
 )
 
 type hexNumber [16]byte
 
 var ring []hexNumber
-var hashToServer map[hexNumber]string
+var hashToServer map[hexNumber]uint32
 var ctx = context.Background()
+
+type Servers struct {
+	serversInfo []ServerInfo `json:"servers"`
+}
+type ServerInfo struct {
+	id          uint32
+	hexLocation string
+}
+
+type ServerInfoDecoded struct {
+	id          uint32
+	hexLocation hexNumber
+}
 
 // Loads the consistent hashing ring from redis into local memory for quick searching
 func Initialise() error {
-	vals, err := redisClient.Client.ZRangeByScore(ctx, "consistentHash", &redis.ZRangeBy{
-		Min: "-inf",
-		Max: "+inf",
-	}).Result()
-
+	data, err := os.ReadFile("../config/serverLocations.txt")
 	if err != nil {
-		return err
+		log.Fatal("can't read the serverLocation config")
 	}
-	ring = make([]hexNumber, len(vals)/2)
-	for i, v := range vals {
-		if i%2 != 0 {
-			hexValue, _ := hex.DecodeString(v)
-			copy(ring[i][:], hexValue)
-			hashToServer[ring[i]] = vals[i-1]
-		}
+	serversEncoded := Servers{}
+	json.Unmarshal(data, &serversEncoded)
+
+	serversDecoded := make([]ServerInfoDecoded, len(serversEncoded.serversInfo))
+
+	for i, info := range serversEncoded.serversInfo {
+		hexValue, _ := hex.DecodeString(info.hexLocation)
+		serversDecoded[i] = ServerInfoDecoded{info.id, hexNumber(hexValue)}
 	}
+
+	slices.SortFunc(serversDecoded, func(a ServerInfoDecoded, b ServerInfoDecoded) int {
+		return bytes.Compare(a.hexLocation[:], b.hexLocation[:])
+	})
+
+	for i, info := range serversDecoded {
+		copy(ring[i][:], info.hexLocation[:])
+		hashToServer[ring[i]] = info.id
+	}
+
 	return nil
 }
 
-func GetServer(key string) string {
+func GetServer(key string) uint32 {
 	serverHex := traverseClockwise(md5.Sum([]byte(key)))
 	return hashToServer[serverHex]
 }
 
-func AddServer() {
+func AddServer(serverId uint32, serverLocation hexNumber) error {
+	data, err := os.ReadFile("../config/serverLocations.txt")
+	if err != nil {
+		log.Fatal("can't read the serverLocation config")
+		return err
+	}
+	serversEncoded := Servers{}
+	json.Unmarshal(data, &serversEncoded)
 
+	serversEncoded.serversInfo = append(serversEncoded.serversInfo, ServerInfo{serverId, hex.EncodeToString(serverLocation[:])})
+
+	marshalledValue, err := json.Marshal(serversEncoded)
+	if err != nil {
+		return err
+	}
+	os.WriteFile("../config/serverLocations.txt", marshalledValue, 0600)
+	return nil
 }
 
 func getAffectedKeys() {
--- consistentHashing/traverse.go
+++ consistentHashing/traverse.go
@@ -5,9 +5,21 @@ import (
 	"sort"
 )
 
-func traverseClockwise(key [16]byte) [16]byte {
+func traverseClockwise(key [16]byte) hexNumber {
 	if bytes.Compare(ring[len(ring)-1][:], key[:]) == 1 { // if key is bigger than the largest then obviously first server would be its target i.e. looping around
 		return ring[0]
+	}
+
+	idx := sort.Search(len(ring), func(idx int) bool {
+		comp := bytes.Compare(ring[idx][:], key[:])
+		return comp == 1 || comp == 0
+	})
+	return ring[idx]
+}
+
+func traverseAntiClockwise(key [16]byte) hexNumber {
+	if bytes.Compare(ring[0][:], key[:]) == -1 { // if key is smaller than the first server, then the last server would be the target i.e. looping around
+		return ring[len(ring)-1]
 	}
 
 	idx := sort.Search(len(ring), func(idx int) bool {
